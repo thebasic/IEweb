9ffaf1ac5fc255330e725cdd622c84bf31a0eac3
ezilrip <thebasic555@gmail.com>
2020-04-21 13:41:13 +0700

    add 2 file
    add startup / program .cs

---
+++ IEweb_Res/IEweb/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace IEweb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
+//Scaffold-DbContext "Server=DESKTOP-H8HM47K;Database=IEDB;UID=AdminSQL;PWD=2535;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
